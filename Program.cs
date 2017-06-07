9847a91ed9038fe81c551a6d758ef9c73a48d4c9
Daniel James <daniel.james@scorebig.com>
2017-06-06 21:44:12 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MqttClient("iot.eclipse.org");
+            client.MqttMsgPublishReceived += (sender, e) =>
+            {
+                Console.WriteLine(System.Text.Encoding.UTF8.GetString(e.Message));
+            };
+            var clientId = Guid.NewGuid().ToString();
+            Console.WriteLine($"Connecting with client ID {clientId}");
+            var connectResult = client.Connect(clientId);
+            if (connectResult == MqttMsgConnack.CONN_ACCEPTED)
+            {
+                Console.WriteLine("Connected!");
+                client.Subscribe(new[] { "/demo/console" }, new[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            }
+            else
+            {
+                Console.Error.WriteLine("Could not connect!");
+            }
+        }
+    }
+}
